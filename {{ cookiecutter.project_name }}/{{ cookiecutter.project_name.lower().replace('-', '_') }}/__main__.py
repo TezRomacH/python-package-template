e6dab80ba89beabd7bb3f2f3af850d0ed134fff6
Tezikov Roman <tez.romach@gmail.com>
2020-04-30 18:22:50 +0300

    fix

--- {{ cookiecutter.project_name }}/{{ cookiecutter.project_name.lower().replace('-', '_') }}/__main__.py
+++ {{ cookiecutter.project_name }}/{{ cookiecutter.project_name.lower().replace('-', '_') }}/__main__.py
@@ -2,11 +2,9 @@ import typer
 
 from {{ cookiecutter.project_name.lower().replace('-', '_') }}.example import hello
 
+app = typer.Typer(help="{{ cookiecutter.project_description }}")
 
-def hello(name: str = typer.Option(..., help="Name of person to greet.")):
+@app.command(name="")
+def main(name: str = typer.Option(..., help="Name of person to greet.")):
     """Greetings for giving name"""
     typer.echo(hello(name))
-
-
-if __name__ == "__main__":
-    typer.run(main)
