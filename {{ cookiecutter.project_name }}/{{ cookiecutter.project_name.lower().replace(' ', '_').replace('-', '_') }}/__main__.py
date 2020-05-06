9f1e763015187074419a4803ed10b37f738d5f1b
Tezikov Roman <tez.romach@gmail.com>
2020-05-06 17:42:25 +0300

    more complex example

--- {{ cookiecutter.project_name }}/{{ cookiecutter.project_name.lower().replace(' ', '_').replace('-', '_') }}/__main__.py
+++ {{ cookiecutter.project_name }}/{{ cookiecutter.project_name.lower().replace(' ', '_').replace('-', '_') }}/__main__.py
@@ -1,14 +1,43 @@
 # type: ignore[attr-defined]
 
-import typer
+import random
+from enum import Enum
+from typing import Optional
 
-from {{ cookiecutter.project_name.lower().replace('-', '_') }}.example import hello
+import typer
+from rich.console import Console
+
+from {{ cookiecutter.project_name.lower().replace(' ', '_').replace('-', '_') }}.example import hello
+
+
+class Color(str, Enum):
+    white = "white"
+    red = "red"
+    cyan = "cyan"
+    magenta = "magenta"
+    yellow = "yellow"
+    green = "green"
+
 
 app = typer.Typer(
     help="{{ cookiecutter.project_description }}"
 )
+console = Console()
+
 
 @app.command(name="")
-def main(name: str = typer.Option(..., help="Name of person to greet.")):
+def main(
+    name: str = typer.Option(..., help="Name of person to greet."),
+    color: Optional[Color] = typer.Option(
+        None,
+        case_sensitive=False,
+        help="Color for name. If not specified then color will be random.",
+    ),
+):
     """Greetings for giving name."""
-    typer.echo(hello(name))
+    if color is None:
+        # If no color specified use random value from `Color` class
+        color = random.choice(list(Color.__members__.values()))
+
+    greeting: str = hello(name)
+    console.print(f"[bold {color}]{greeting}[/]")
