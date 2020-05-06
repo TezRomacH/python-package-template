8bb457749df3b46429d8a0fb634a9b161deed80a
Tezikov Roman <tez.romach@gmail.com>
2020-05-06 22:49:25 +0300

    added `version` to examples

--- {{ cookiecutter.project_name }}/{{ cookiecutter.project_name.lower().replace(' ', '_').replace('-', '_') }}/__main__.py
+++ {{ cookiecutter.project_name }}/{{ cookiecutter.project_name.lower().replace(' ', '_').replace('-', '_') }}/__main__.py
@@ -7,6 +7,7 @@ from typing import Optional
 import typer
 from rich.console import Console
 
+from {{ cookiecutter.project_name.lower().replace(' ', '_').replace('-', '_') }} import __version__
 from {{ cookiecutter.project_name.lower().replace(' ', '_').replace('-', '_') }}.example import hello
 
 
@@ -20,9 +21,20 @@ class Color(str, Enum):
 
 
 app = typer.Typer(
-    help="{{ cookiecutter.project_description }}"
+    name="{{ cookiecutter.project_name }}",
+    help="{{ cookiecutter.project_description }}",
+    add_completion=False,
 )
 console = Console()
+
+
+def version_callback(value: bool):
+    """Prints the version of the package."""
+    if value:
+        console.print(
+            f"[yellow]{{ cookiecutter.project_name }}[/] version: [bold blue]{__version__}[/]"
+        )
+        raise typer.Exit()
 
 
 @app.command(name="")
@@ -31,10 +43,17 @@ def main(
     color: Optional[Color] = typer.Option(
         None,
         case_sensitive=False,
-        help="Color for name. If not specified then color will be random.",
+        help="Color for name. If not specified then choice will be random.",
+    ),
+    version: bool = typer.Option(
+        None,
+        "--version",
+        callback=version_callback,
+        is_eager=True,
+        help="Prints the version of the {{ cookiecutter.project_name }} package.",
     ),
 ):
-    """Greetings for giving name."""
+    """Prints a greeting for a giving name."""
     if color is None:
         # If no color specified use random value from `Color` class
         color = random.choice(list(Color.__members__.values()))
