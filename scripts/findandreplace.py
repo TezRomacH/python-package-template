b2e5d78a3b3f807a42509cc2c1bf420d56722916
william-cass-wright <william.cass.wright@gmail.com>
2022-07-18 00:34:38 -0700

    find and replace for json, py, and toml files

--- scripts/findandreplace.py
+++ scripts/findandreplace.py
@@ -1,12 +1,16 @@
 """
 https://stackoverflow.com/questions/4205854/python-way-to-recursively-find-and-replace-string-in-text-files
+
+** MAKE SURE TO RUN THIS SCRIPT FROM THE SCRIPTS DIRECTORY **
 """
 
 import os, fnmatch
 
 
 def find_and_replace(directory, find, replace, filePattern):
-    for path, dirs, files in os.walk(os.path.abspath(directory)):
+    abs_dir = os.path.abspath(directory)
+    print(abs_dir)
+    for path, dirs, files in os.walk(abs_dir):
         for filename in fnmatch.filter(files, filePattern):
             filepath = os.path.join(path, filename)
             with open(filepath) as f:
@@ -18,8 +22,8 @@ def find_and_replace(directory, find, replace, filePattern):
 
 if __name__ == "__main__":
     find_and_replace(
-        ".",
-        "TezRomacH/python-package-template",
+        "../.",
         "william-cass-wright/cookiecutter-pypackage-slim",
-        "*.md",
+        "william-cass-wright/cookiecutter-pypackage-slim",
+        "*.toml", # "*.md", "*.json", "*.toml",
     )
